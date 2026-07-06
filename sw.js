f97b4b645e2ac11f7b43c507af7a4d5fc9f9661b
Dear-linko <kariyawasamsingh805@gmail.com>
2026-07-06 23:59:53 +0800

    Publish offline entry shell (static PWA)

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "sc-inf-shell-837e468eac1f";
+self.addEventListener("install", (event) => {
+    event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["./"])));
+    self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+    event.waitUntil(caches.keys().then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+});
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") return;
+    event.respondWith(
+        fetch(event.request).then((response) => {
+            if (response.ok) {
+                const copy = response.clone();
+                caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+            }
+            return response;
+        }).catch(() => caches.match(event.request).then((hit) => hit || caches.match("./")))
+    );
+});
